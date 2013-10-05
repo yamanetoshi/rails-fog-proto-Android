cece66738d4601d5304f59ea183a353c5d169846
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-05 13:52:19 +0900

    Implement login function

---
+++ src/jp/shuri/railsclient/MyProgressDialog.java
@@ -0,0 +1,17 @@
+package jp.shuri.railsclient;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+public class MyProgressDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    	ProgressDialog progressDialog = new ProgressDialog(getActivity());
+    	progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+    	progressDialog.setMessage("Please wait...");
+
+        return progressDialog;
+    }
+}
