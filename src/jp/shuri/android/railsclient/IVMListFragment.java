04ad30a4c2fe914ee1df552ed26ae67eab9fef6e
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-11 11:18:20 +0900

    Fix Issue#40

--- src/jp/shuri/android/railsclient/IVMListFragment.java
+++ src/jp/shuri/android/railsclient/IVMListFragment.java
@@ -1,5 +1,7 @@
 package jp.shuri.android.railsclient;
 
+import jp.shuri.android.railsclient.VMListFragment.Operation;
+
 public interface IVMListFragment {
-	public void kickoff(final boolean b, final int i);
+	public void kickoff(final boolean b, final int i, Operation ope);
 }
